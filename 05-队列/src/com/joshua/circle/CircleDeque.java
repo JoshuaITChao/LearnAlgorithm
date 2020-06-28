86fa8efb245021dcb90055ce3c35b78bf2aa469a
JoshuaITChao <JoshuaITChao@163.com>
2020-06-28 13:50:19 +0800

    算法学习
    1.队列学习

--- 05-队列/src/com/joshua/circle/CircleDeque.java
+++ 05-队列/src/com/joshua/circle/CircleDeque.java
@@ -1,13 +1,14 @@
 package com.joshua.circle;
 
 @SuppressWarnings("unchecked")
-public class CircleQueue<E> {
+public class CircleDeque<E> {
+	
 	private int front;
 	private int size;
 	private E[] elements;
 	private static final int DEFAULT_CAPACITY = 10;
 	
-	public CircleQueue() {
+	public CircleDeque() {
 		elements = (E[])new Object[DEFAULT_CAPACITY];
 	}
 	public int size() {
@@ -16,25 +17,64 @@ public class CircleQueue<E> {
 	public boolean isEmpty() {
 		return size == 0;
 	}
-	public void enQueue(E element) {
+	
+	/**
+	 * 从尾部入队
+	 * @param element
+	 */
+	public void enQueueRear(E element) {
 		ensureCapacity(size+1);
 		
 		elements[index(size)] = element;
 		size++;
 	}
-	public E deQuque() {
+	/**
+	 * 从头部出队
+	 * @return
+	 */
+	public E deQuqueFront() {
 		E frontElement = elements[front];
 		elements[front] = null;
 		front = index(1);
 		size--;
 		return frontElement;
 	}
+	
+	/**
+	 * 从头部入队
+	 * @param element
+	 */
+	public void enQueueFront(E element) {
+		ensureCapacity(size+1);
+		
+		front = index(-1);
+		elements[front] = element;
+		size++;
+	}
+	/**
+	 * 从尾部出队
+	 * @return
+	 */
+	public E deQueueRear() {
+		int rearIndex = index(size-1);
+		E rearElement = elements[rearIndex];
+		elements[rearIndex] = null;
+		size--;
+		return rearElement;
+	}
+	
 	public E fornt() {
 		return elements[front];
+	}
+	public E rear() {
+		return elements[index(size - 1)];
 	}
 	
 	private int index(int index) {
 		index += front;
+		if(index < 0) {
+			index = index + elements.length;
+		}
 		// 模拟 (index + front) % elements.length
 		return index - (index >= elements.length ? elements.length : 0);
 	}
